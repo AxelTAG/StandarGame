c6f7f6dd081eb3095cba6ac16f2f98302fad679e
Kevin Axel Tagliaferri <kevinaxeltagliaferri@gmail.com>
2024-10-25 11:14:46 -0300

    Implementig attr(define and field) to class Player.

--- player.py
+++ player.py
@@ -6,109 +6,91 @@ from inventory import Inventory
 
 # External imports.
 import numpy as np
-from datetime import timedelta
+from attr import define, field
+from datetime import timedelta, datetime
 
 
 # Player class.
+@define
 class Player:
-    def __init__(self,
-                 name: str = "",
-                 hp: int = 25,
-                 hpmax: int = 25,
-                 lvl: int = 1,
-                 exp: int = 0,
-                 expmax: int = 10,
-                 attack: int = 2,
-                 defense: int = 1,
-                 evasion: int = 0,
-                 precision: int = 60,
-                 b_hpmax: int = 25,
-                 b_attack: int = 2,
-                 b_defense: int = 1,
-                 b_evasion: int = 0,
-                 b_precision: int = 0.6,
-                 strength: int = 0,
-                 agility: int = 0,
-                 vitality: int = 0,
-                 resistance: int = 0,
-                 dexterity: int = 0,
-                 x: int = 0,
-                 y: int = 0,
-                 x_cp: int = 0,
-                 y_cp: int = 0,
-                 st_points: int = 0,
-                 sk_points: int = 0,
-                 slot1: str = "red_potion",
-                 slot2: str = "litle_red_potion",
-                 equip: dict = None,
-                 inventory: Inventory = Inventory(),
-                 status: int = PlayerStatus.WALK.value,
-                 outside: bool = False,
-                 place: Biome | Entry = None,
-                 explored_map: np.array = None,
-                 poison: int = 0):
+    # Player name.
+    name: str = field(default="")
 
-        if len(name) > 12:
-            self.name = name
-        else:
-            self.name = name[:12]
+    # Player current attributes.
+    hp: int = field(default=25)  # Hit points or life.
+    hpmax: int = field(default=25)  # Max hit points or max life.
+    lvl: int = field(default=1)  # Level of player.
+    exp: int = field(default=0)  # Actual experience points.
+    expmax: int = field(default=10)  # Max experience points for level up.
 
-        # Status of player attributes.
-        self.hp = hp  # Hit points or life.
-        self.hpmax = hpmax  # Max hit points or max life.
-        self.lvl = lvl  # Level of player.
-        self.exp = exp  # Actual experience points.
-        self.expmax = expmax  # Max experience points for level up.
-        self.attack = attack  # Current attack of player (base stats + equipment + others).
-        self.defense = defense  # Current defense of player (base stats + equipment + others).
-        self.evasion = evasion  # Current evasion of player (base stats + equipment + others).
-        self.precision = precision  # Current precision of player (base stats + equipment + others).
-        self.status = status  # 0: walk, 1: surf.
-        self.poison = poison
+    attack: int = field(default=2)  # Current attack of player (base stats + equipment + others).
+    defense: int = field(default=1)  # Current defense of player (base stats + equipment + others).
+    evasion: int = field(default=0)  # Current evasion of player (base stats + equipment + others).
+    precision: int = field(default=0)  # Current precision of player (base stats + equipment + others).
 
-        # Stats of player attributes.
-        self.b_hpmax = b_hpmax
-        self.b_attack = b_attack
-        self.b_defense = b_defense
-        self.b_evasion = b_evasion
-        self.b_precision = b_precision
-        self.strength = strength
-        self.agility = agility
-        self.vitality = vitality
-        self.resistance = resistance
-        self.dexterity = dexterity
-        self.st_points = st_points
-        self.sk_points = sk_points
+    status: int = field(default=PlayerStatus.WALK.value)
+    poison: int = field(default=0)
 
-        # Placement player and map attributes.
-        self.x = x
-        self.y = y
-        self.x_cp = x_cp
-        self.y_cp = y_cp
-        self.outside = outside
-        self.place = place
+    # Player basis attributes.
+    b_hpmax: int = field(default=25)
+    b_attack: int = field(default=2)
+    b_defense: int = field(default=1)
+    b_evasion: float = field(default=0)
+    b_precision: float = field(default=0.6)
 
-        if explored_map is None:
+    # Stats attributes.
+    strength: int = field(default=0)
+    agility: int = field(default=0)
+    vitality: int = field(default=0)
+    resistance: int = field(default=0)
+    dexterity: int = field(default=0)
+
+    # Player location attributes.
+    x: int = field(default=0)
+    y: int = field(default=0)
+    x_cp: int = field(default=0)
+    y_cp: int = field(default=0)
+    outside: bool = field(default=False)
+    place: Biome | Entry = field(default=None)
+
+    # Lvl up attributes.
+    st_points: int = field(default=0)
+    sk_points: int = field(default=0)
+
+    # Inventory attributes.
+    inventory: Inventory = field(default=None)
+    slot1: str = field(default="red_potion")
+    slot2: str = field(default="litle_red_potion")
+    equip: dict = field(default=None)
+
+    # Map attributes.
+    explored_map: np.array = field(default=None)
+
+    # Others.
+    events: dict = field(default=None)
+    time_played: timedelta = field(default=timedelta(seconds=0))
+
+    def __attrs_post_init__(self):
+        if len(self.name) > 12:
+            self.name = self.name[:12]
+
+        if self.inventory is None:
+            self.inventory = Inventory()
+
+        if self.explored_map is None:
             self.map = np.zeros(shape=(32, 32, 4), dtype=np.uint8)
             self.map[:, :, 3] = np.ones(shape=(32, 32), dtype=np.uint8) * 255
 
-        # Inventory and equip player attributes.
-        self.slot1 = slot1
-        self.slot2 = slot2
-        self.equip = equip
-
-        if equip is None:
+        if self.equip is None:
             self.equip = {"head": "None", "chest": "None", "right_hand": "None", "left_hand": "None", "legs": "None"}
+
         else:
-            # Asegurarse de que equip sea un diccionario con las claves correctas
+            # Make sure equip is a dictionary with the correct keys.
             valid_keys = {"head", "chest", "right_hand", "left_hand", "legs"}
-            self.equip = {key: equip.get(key, "None") for key in valid_keys}
+            self.equip = {key: self.equip.get(key, "None") for key in valid_keys}
 
-        self.inventory = inventory
-        self.events = {"message": False, "permission": False, "win": False}
-
-        # Other player attributes.
-        self.time_played = timedelta(seconds=0)
+        if self.events is None:
+            self.events = {"message": False, "permission": False, "win": False}
 
     def heal(self, amount: int) -> None:
         if self.hp + amount < self.hpmax:
@@ -121,7 +103,7 @@ class Player:
         if self.poison:
             self.poison = 0
 
-    def refresh_time_played(self, time_close: int, time_init: int):
+    def refresh_time_played(self, time_close: timedelta | datetime, time_init: timedelta | datetime):
         self.time_played = time_close - time_init + self.time_played
 
     def refresh_status(self):

d4d4d66cd2a9930f5f253b4540b1d62c73eebaf4
Kevin Axel Tagliaferri <kevinaxeltagliaferri@gmail.com>
2025-10-29 11:14:38 -0300

    Refactor of use action. Adapting of Item class at this action, and add of little_strength_potion use.

--- src/actions/use.py
+++ src/actions/use.py
@@ -1,60 +1,62 @@
 # Imports.
 # Local imports.
+from ..enums import StatType
 from ..item import Item
 from ..player import Player
 from ..map import Map
+from ..world import BUFFS
 
 
-def use(player: Player, mapgame: Map = None, item: str | Item = None) -> tuple[str, bool]:
-    if isinstance(item, Item):
-        item = item.id
+def use(player: Player,
+        mapgame: Map = None,
+        item: str | Item = None) -> tuple[str, bool]:
+    if isinstance(item, str):
+        item = player.inventory.get_item_object(item=item)
 
     if item is None:
-        return "You cannot use None.", False
+        return "You cannot use this.", False
 
-    if item == "gold":
-        return f"You can't use {item.replace('_', ' ').title()}.", False
+    if item.id == "gold":
+        return f"You can't use {item.name.title()}.", False
 
-    if item not in player.inventory.items.keys():
-        return f"You have no {item.replace('_', ' ').title()}.", False
+    if not player.has(item=item.id, amount=1):
+        return f"You have no {item.name.title()}.", False
 
-    if player.inventory.items[item] > 0:
-        if "potion" in item:
-            if item == "giant_red_potion":
-                player.heal(amount=40)
+    # Potions.
+    if item.id == "little_red_potion":
+        player.heal(amount=10)
+        player.inventory.discard_item(item=item.id, quantity=1)
+        return f"{player.name}'s HP refilled to {player.hp}!", True
 
-            elif item == "red_potion":
-                player.heal(amount=25)
+    if item.id == "red_potion":
+        player.heal(amount=25)
+        player.inventory.discard_item(item=item.id, quantity=1)
+        return f"{player.name}'s HP refilled to {player.hp}!", True
 
-            elif item == "little_red_potion":
-                player.heal(amount=10)
+    if item.id == "giant_red_potion":
+        player.heal(amount=40)
+        player.inventory.discard_item(item=item.id, quantity=1)
+        return f"{player.name}'s HP refilled to {player.hp}!", True
 
-            else:
-                return "Nothing done.", False
+    if item.id == "antidote":
+        player.heal_poisoning(amount=3)
+        player.inventory.discard_item(item=item.id, quantity=1)
+        return "You have taken the antidote.", True
 
-            player.inventory.discard_item(item=item, quantity=1)
+    if item.id == "little_strength_potion":
+        player.add_buff(buff=BUFFS[item.id])
+        player.inventory.discard_item(item=item.id, quantity=1)
+        return "You feel a small surge of strength.", True
 
-            return f"{player.name}'s HP refilled to {player.hp}!", True
-
-        elif "antidote" in item:
-            player.heal_poisoning()
-            player.inventory.discard_item(item=item, quantity=1)
-
-            return "You have taken the antidote.", True
-
-        elif "powder_keg" in item:
-            explosion = False
-            for neighbor in mapgame.neighbors_from_coord((player.x, player.y)):
-                if "rocks" in neighbor.req:
-                    neighbor.req.remove("rocks")
-                    player.inventory.discard_item(item=item, quantity=1)
-                    explosion = True
-            if explosion:
-                return "You have explode the keg powder on all the rocks.", True
-            else:
-                return "There is nothing to explode here.", False
-
-        else:
-            return "You can't use this item.", False
-    else:
-        return f"You have no more {item.replace('_', ' ').title()}.", False
+    # Others.
+    if item.id == "powder_keg":
+        explosion = False
+        for neighbor in mapgame.neighbors_from_coord((player.x, player.y)):
+            if "rocks" in neighbor.req:
+                neighbor.req.remove("rocks")
+                player.inventory.discard_item(item=item.id, quantity=1)
+                explosion = True
+        if explosion:
+            return "You have explode the keg powder on all the rocks.", True
+        return "There is nothing to explode here.", False
+    return "You can't use this item.", False
